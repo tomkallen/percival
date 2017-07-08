fac67dca1b386bbe390715c46902faf93b77d51b
Alexei Bykov <spbeat@mail.ru>
2017-07-08 20:33:21 +0300

    direct messages

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const emoji = require('node-emoji').emoji;
 const users = [];
 
 bot.on('message', (msg) => {
+    
     if (users.indexOf(msg.from.id) === -1) {
         bot.sendMessage(msg.from.id, "Hello " + "<b>" + msg.from.first_name + "</b>, nice to meet you! You are new here, right? I am Percival, your new friend, lets get to know each other!", {
             parse_mode: "HTML"
         });
         users.push(msg.from.id);
         console.log(`New user ${msg.from.first_name} with and id ${msg.from.id} joined the bot`);
+        
     } else {
         bot.sendMessage(msg.from.id, "Hello again " + "<b>" + msg.from.first_name + "</b>, how are you today? " + emoji.heart, {
             parse_mode: "HTML"
@@ -29,3 +31,9 @@ bot.onText(/\/start/, (msg) => {
         }
     });
 });
+
+bot.onText(/\/love/, (msg) => {
+    bot.sendMessage(128056504, "Я люблю зайца!");
+});
+
+bot.sendMessage(165205553, "Сервер с ботом запущен");
